677aa2c6cf70c140a2b1add265c65ee90b7a794f
Mohammad Khajah <sw2x@Mohammads-MacBook-Pro.local>
2021-04-29 22:52:13 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import Swal from 'sweetalert2';
 
 function App() {
+
+  const Course = [
+    {
+      name: 'Math',
+      Teacher: 'Mohammad',
+      filename: 'Math.pdf',
+      bg: '#FFFFF',
+      color: '#000'
+    },
+    {
+      name: 'English',
+      Teacher: 'Mohammad',
+      filename: 'English.pdf',
+      bg: 'yellow',
+      color: '#FFFFF'
+    },
+    {
+      name: 'Arabic',
+      Teacher: 'Mohammad',
+      filename: 'Arabic.pdf',
+      bg: 'blue',
+      color: '#FFFFF'
+    }
+  ]
+
+  const showDetails = (name) => {
+    let cc = Course.filter(x => x.name == name)[0];
+
+    Swal.fire({
+      title: `<strong>${cc.name}</strong>`,
+      html: `<br /> <h3>Instructor: <strong>${cc.Teacher}</strong></h3><br /> <h3>Download files: </h3><br /> <a href='${cc.filename}' download>File 1</a> <br /> <a href='${cc.filename}' download>File 2</a> <br /> <a href='${cc.filename}' download>File 3</a> <br /> <a href='${cc.filename}' download>File 4</a> <br />`,
+      confirmButtonText:
+        '<i class="fa fa-thumbs-up"></i> Ok!',
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="wrapper">
+    <header>
+     <h1>Cointify</h1>
+     </header>
+     <div className="container">
+       <div className="container2" id="container">
+         {
+           Course.map((item) => {
+             return <div className="box" style={{backgroundColor: item.bg , color: item.color}} onClick={() => {showDetails(item.name)}}><h2>{item.name}</h2></div>
+           })
+         }
+       </div>
+     </div>
+ </div>
   );
 }
 
---
+++ src/Math.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Math() {
+    return (
+        <div>
+            <h1>sss</h1>
+        </div>
+    )
+}
+
+export default Math
